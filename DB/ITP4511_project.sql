8cec48934bc99da33399daaa47aa659ed6ec02d3
Eunice000 <chiuchungyiu@gmail.com>
2023-04-28 02:38:55 +0800

    update

--- DB/ITP4511_project.sql
+++ DB/ITP4511_project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 26, 2023 at 07:26 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost
+-- Generation Time: Apr 27, 2023 at 08:37 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `itp4511_project`
+-- Database: `ITP4511_project`
 --
 
 -- --------------------------------------------------------
@@ -38,26 +38,24 @@ CREATE TABLE `booking` (
   `Hour` int(10) NOT NULL,
   `RequestState` varchar(20) NOT NULL DEFAULT 'processing',
   `CheckState` varchar(20) NOT NULL DEFAULT 'waiting'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `booking`
 --
 
 INSERT INTO `booking` (`BkingID`, `VenueID`, `MemberID`, `Fee`, `BkDate`, `StartTime`, `EndTime`, `Hour`, `RequestState`, `CheckState`) VALUES
-(1, 1001, 3, 12312, '2023-04-19', '11:31:03', '11:31:03', 2, 'processing', 'waiting'),
-(2, 122, 3127, 123, '2023-04-19', '11:32:08', '11:32:08', 2, 'processing', 'waiting'),
-(3, 1004, 2, 12, '2023-04-27', '00:00:00', '12:00:00', 2, 'processing', 'waiting'),
+(1, 10001, 3, 12312, '2023-04-19', '11:30:00', '11:30:00', 2, 'completed', 'waiting'),
+(2, 122, 3127, 123, '2023-04-19', '11:32:08', '11:32:08', 2, 'completed', 'waiting'),
 (4, 213, 3, 123, '2023-04-26', '00:00:12', '00:00:21', 21, 'processing', 'waiting'),
 (5, 213, 3, 123, '2023-04-26', '00:00:12', '00:00:21', 21, 'processing', 'waiting'),
-(12, 12, 2, 12, '0000-00-00', '00:00:12', '00:00:21', 12, 'processing', 'waiting'),
+(12, 12, 2, 12, '0000-00-00', '00:00:12', '00:00:21', 12, 'completed', 'waiting'),
 (13, 21, 3, 12, '2023-04-26', '12:27:56', '12:27:56', 3, 'processing', 'waiting'),
 (15, 12, 3127, 12, '2023-04-19', '12:29:48', '12:29:48', 12, 'processing', 'waiting'),
-(17, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, '[value-9]', '[value-10]'),
 (18, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'waiting'),
-(19, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, '', ''),
-(20, 10001, 2, 1100, '2023-04-26', '01:00:00', '12:00:00', 11, 'processing', 'wating'),
-(21, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
+(19, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'completed', 'cancel'),
+(20, 10001, 2, 1100, '2023-04-26', '01:00:00', '12:00:00', 11, 'completed', 'check-out'),
+(21, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'completed', 'cancel'),
 (22, 10001, 2, 2530, '2023-04-19', '02:00:00', '13:00:00', 11, 'processing', 'wating'),
 (23, 10001, 2, 2530, '2023-04-19', '02:00:00', '13:00:00', 11, 'processing', 'wating'),
 (24, 21, 3, 12, '2023-04-26', '15:13:33', '15:13:33', 3, 'processing', 'waiting'),
@@ -77,22 +75,7 @@ INSERT INTO `booking` (`BkingID`, `VenueID`, `MemberID`, `Fee`, `BkDate`, `Start
 (47, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
 (48, 10005, 2, 2760, '2023-04-26', '02:00:00', '14:00:00', 12, 'processing', 'wating'),
 (49, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(50, 10005, 2, 2760, '2023-04-19', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(51, 10005, 2, 2990, '2023-04-11', '01:00:00', '14:00:00', 13, 'processing', 'wating'),
-(52, 10005, 2, 2990, '2023-04-27', '01:00:00', '14:00:00', 13, 'processing', 'wating'),
-(53, 10001, 2, 1200, '2023-04-19', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(54, 10005, 2, 2760, '2023-04-26', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(55, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(56, 10005, 2, 2760, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(57, 10001, 2, 1100, '2023-04-27', '01:00:00', '12:00:00', 11, 'processing', 'wating'),
-(58, 10001, 2, 1200, '2023-04-27', '01:00:00', '13:00:00', 12, 'processing', 'wating'),
-(59, 10001, 2, 1300, '2023-04-27', '01:00:00', '14:00:00', 13, 'processing', 'wating'),
-(60, 10005, 2, 2760, '2023-04-27', '02:00:00', '14:00:00', 12, 'processing', 'wating'),
-(61, 10005, 2, 2760, '2023-04-27', '02:00:00', '14:00:00', 12, 'processing', 'wating'),
-(62, 10005, 2, 2760, '2023-04-27', '02:00:00', '14:00:00', 12, 'processing', 'wating'),
-(63, 10001, 2, 1100, '2023-04-26', '02:00:00', '13:00:00', 11, 'processing', 'wating'),
-(64, 10001, 2, 2760, '2023-04-27', '02:00:00', '14:00:00', 12, 'processing', 'wating'),
-(65, 10005, 2, 2530, '2023-04-28', '01:00:00', '12:00:00', 11, 'processing', 'wating');
+(50, 10005, 2, 2760, '2023-04-19', '01:00:00', '13:00:00', 12, 'processing', 'wating');
 
 -- --------------------------------------------------------
 
@@ -105,20 +88,20 @@ CREATE TABLE `bookingfee` (
   `Year` year(4) NOT NULL,
   `VenueID` int(5) NOT NULL,
   `Fee` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bookingfee`
 --
 
 INSERT INTO `bookingfee` (`FeeID`, `Year`, `VenueID`, `Fee`) VALUES
-(10001, '2022', 10001, 100),
-(10002, '2022', 10004, 250),
-(10003, '2023', 10005, 230),
-(10004, '2023', 10001, 100),
-(10005, '2023', 10002, 150),
-(10006, '2023', 10003, 110),
-(10007, '2023', 10004, 125);
+(10001, 2022, 10001, 100),
+(10002, 2022, 10004, 250),
+(10004, 2023, 10001, 100),
+(10005, 2023, 10002, 150),
+(10006, 2023, 10003, 110),
+(10007, 2023, 10004, 125),
+(10008, 2023, 10005, 230);
 
 -- --------------------------------------------------------
 
@@ -131,7 +114,7 @@ CREATE TABLE `guest` (
   `BkingID` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `guest`
@@ -139,8 +122,13 @@ CREATE TABLE `guest` (
 
 INSERT INTO `guest` (`GuestID`, `BkingID`, `Name`, `Email`) VALUES
 (1, 22, 'Ken', 'ken@gmail.com'),
+(2, 39, 'ken@gmail.com', 'ken@gmail.com'),
 (3, 39, 'Peter@gmail.com', 'Peter@gmail.com'),
+(4, 39, 'ken@gmail.com', 'ken@gmail.com'),
+(5, 39, 'Peter@gmail.com', 'Peter@gmail.com'),
 (6, 40, 'peter@gmail.com', 'peter@gmail.com'),
+(7, 40, '21321@gmail.com', '21321@gmail.com'),
+(8, 41, 'Ken', '123@gmail.com'),
 (9, 41, 'Kan', '12312@gmail.com'),
 (10, 47, 'Ken', 'ken@gmail.com'),
 (11, 48, 'Ken', 'ken@gmail.com'),
@@ -155,7 +143,7 @@ INSERT INTO `guest` (`GuestID`, `BkingID`, `Name`, `Email`) VALUES
 CREATE TABLE `management` (
   `MID` varchar(20) NOT NULL,
   `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `management`
@@ -179,14 +167,14 @@ CREATE TABLE `user` (
   `email` varchar(30) NOT NULL,
   `PhoneNum` int(10) NOT NULL,
   `role` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`UID`, `Name`, `Password`, `email`, `PhoneNum`, `role`) VALUES
-(2, 'Peter', '12345678', 'Peter@gmail.com', 123456232, 'Member'),
+(2, 'Peter', '12345678', 'Peter@gmail.com', 12345623, 'Member'),
 (3, 'Dan', '12345678', 'Dan@gmail.com', 1234231, 'Manager'),
 (3127, 'Ken', '12345678', '21321312', 213123, 'Staff'),
 (3131, '1234512', 'a52262983', 'kem@gmail.com', 3213, 'Member');
@@ -198,7 +186,7 @@ INSERT INTO `user` (`UID`, `Name`, `Password`, `email`, `PhoneNum`, `role`) VALU
 --
 
 CREATE TABLE `venue` (
-  `VenueID` int(5) NOT NULL,
+  `VenueID` int(11) NOT NULL,
   `VenueName` varchar(50) NOT NULL,
   `Img` mediumblob DEFAULT NULL,
   `VenueType` varchar(50) NOT NULL,
@@ -207,17 +195,17 @@ CREATE TABLE `venue` (
   `VenueDesc` varchar(50) NOT NULL,
   `VenuePerson` varchar(50) NOT NULL,
   `State` varchar(20) NOT NULL DEFAULT 'Open'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `venue`
 --
 
 INSERT INTO `venue` (`VenueID`, `VenueName`, `Img`, `VenueType`, `Capacity`, `Location`, `VenueDesc`, `VenuePerson`, `State`) VALUES
-(10001, 'Tuen Mun ', '', 'Sports Ground', 1000, 'Tuen Mun, Hong Kong', 'recreational venue', 'Eunice', 'Close'),
-(10002, 'Sha Tin', '', 'Room', 10, 'Sha Tin, Hong Kong', '1', 'Eunice', 'Close'),
+(10001, 'Tuen Mun ', '', 'Sports Ground', 1000, 'Tuen Mun, Hong Kong', 'recreational venue', 'Teddy', 'Close'),
+(10002, 'Sha Tin', '', 'Stadium', 5000, 'Sha Tin, Hong Kong', '1', 'Eunice', 'Close'),
 (10003, 'Tsing Yi', NULL, 'Hall', 2000, 'Tsing Yi, Hong Kong', 'Big', 'Eunice', 'Open'),
-(10004, 'Lee Wai Lee', NULL, '12', 133, 'Lee Wai Lee, Hong Kong', 'Bar ', 'Eunice', 'Open'),
+(10004, 'Lee Wai Lee', NULL, 'Bar', 1500, 'Lee Wai Lee, Hong Kong', 'Bar ', 'Alex', 'Open'),
 (10005, 'Chai Wan', '', 'Room', 4000, 'Chai Wan, Hong Kong', 'HK', 'Eunice', 'Open');
 
 --
@@ -229,14 +217,14 @@ INSERT INTO `venue` (`VenueID`, `VenueName`, `Img`, `VenueType`, `Capacity`, `Lo
 --
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`BkingID`),
-  ADD KEY `mk` (`MemberID`);
+  ADD KEY `mk` (`MemberID`),
+  ADD KEY `VenueID` (`VenueID`);
 
 --
 -- Indexes for table `bookingfee`
 --
 ALTER TABLE `bookingfee`
-  ADD PRIMARY KEY (`FeeID`),
-  ADD KEY `vID_fk` (`VenueID`);
+  ADD PRIMARY KEY (`FeeID`);
 
 --
 -- Indexes for table `guest`
@@ -275,13 +263,13 @@ ALTER TABLE `venue`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `BkingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `BkingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `bookingfee`
 --
 ALTER TABLE `bookingfee`
-  MODIFY `FeeID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10008;
+  MODIFY `FeeID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10009;
 
 --
 -- AUTO_INCREMENT for table `guest`
@@ -299,7 +287,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `venue`
 --
 ALTER TABLE `venue`
-  MODIFY `VenueID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10009;
+  MODIFY `VenueID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10009;
 
 --
 -- Constraints for dumped tables
